325fef3c0c8bdcfa2cd58fcf13634881036d6141
Anil Tigga <57623270+anighost1@users.noreply.github.com>
2024-10-16 16:00:07 +0530

    monthly statistics

--- src/Components/RMC_admin/RMC_Dashboard.jsx
+++ src/Components/RMC_admin/RMC_Dashboard.jsx
@@ -27,7 +27,14 @@ export default function RMC_Dashboard() {
   const [ntoDates, setNToDates] = useState(new Date());
 
   const [hourlyRealTimeData, setHourlyRealTimeData] = useState([]);
+  const [statisticsData, setStatisticsData] = useState([]);
+  const categories = statisticsData?.map((item) => new Date(item?.date).toISOString().split('T')[0]);
+  const totalAmount = statisticsData?.map((item) => item?.total_amount);
+  const totalAmountSum = totalAmount?.reduce((sum, item) => sum + item, 0);
+  const totalBill = statisticsData?.map((item) => item?.vehicle_count);
+  const totalBillSum = totalBill?.reduce((sum, item) => sum + item, 0);
   const token = Cookies.get("accesstoken");
+
 
   /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -358,7 +365,33 @@ export default function RMC_Dashboard() {
       .catch((err) => {
         console.log(err);
       });
+
   }, [])
+
+
+  useEffect(() => {
+    axios
+      .post(
+        `${process.env.REACT_APP_BASE_URL}/report/statistics`,
+        {
+          fromDates,
+          toDates,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      )
+      .then((res) => {
+        console.log(res, 'category')
+        setStatisticsData(res.data?.data?.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [fromDates, toDates]);
+
 
   const currentDate = formatDate(new Date());
 
@@ -434,6 +467,59 @@ export default function RMC_Dashboard() {
     },
   };
 
+  const barchartOptions = {
+    chart: {
+      type: "line",
+      toolbar: {
+        show: false,
+      },
+    },
+    plotOptions: {
+      bar: {
+        horizontal: false,
+        columnWidth: "90%",
+        endingShape: "rounded",
+      },
+    },
+
+    colors: ["#00599C", "#1A91C1", "#01D8FF", "#00599C"],
+    xaxis: {
+      categories: categories,
+    },
+    legend: {
+      show: true,
+      position: 'bottom',
+      horizontalAlign: 'center',
+      labels: {
+        useSeriesColors: true // Use the colors defined in the series
+      }
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    series: [
+      {
+        name: "Total amount",
+        data: totalAmount,
+      },
+      {
+        name: "Total bill cut",
+        data: totalBill,
+      },
+    ],
+    toolbar: {
+      tools: {
+        download: true,
+      },
+    },
+    markers: {
+      size: 5,
+    },
+    stroke: {
+      curve: "smooth",
+    },
+  };
+
 
   return (
     <>
@@ -450,7 +536,7 @@ export default function RMC_Dashboard() {
             <div className="flex flex-row justify-end item-center mx-10 ">
               <Button
                 variant="contained"
-                onClick={()=>{navigate('/collection-report')}}
+                onClick={() => { navigate('/collection-report') }}
               >
                 Collection Report
               </Button>
@@ -848,6 +934,71 @@ export default function RMC_Dashboard() {
                 </div>
               </div>
             </div>
+
+            <div className="flex flex-1 justify-center items-center">
+              <div className={` w-full h-auto mx-5 my-5 flex flex-col overflow-auto relative bg-[#fff] p-5 shadow-lg rounded-md`}>
+                <div className="flex items-center justify-between text-xl">
+                  <div className="flex items-center">
+                    <i className="mr-2">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="32"
+                        height="32"
+                        viewBox="0 0 32 32"
+                        fill="none"
+                      >
+                        <rect width="32" height="32" rx="9" fill="#665DD9" />
+                        <path
+                          d="M19.6367 6C23.4494 6 25.84 8.37312 25.84 12.2033V14.5066L25.8331 14.6096C25.7828 14.9801 25.4652 15.2656 25.0809 15.2656H25.0722L24.9524 15.256C24.7948 15.2306 24.6484 15.1554 24.5354 15.0397C24.3942 14.8952 24.3172 14.6999 24.3219 14.4979V12.2033C24.3219 9.18452 22.6555 7.5181 19.6367 7.5181H12.2033C9.1758 7.5181 7.5181 9.18452 7.5181 12.2033V19.6455C7.5181 22.6642 9.18452 24.3219 12.2033 24.3219H19.6367C22.6642 24.3219 24.3219 22.6555 24.3219 19.6455C24.3219 19.2262 24.6617 18.8864 25.0809 18.8864C25.5002 18.8864 25.84 19.2262 25.84 19.6455C25.84 23.4669 23.4669 25.84 19.6455 25.84H12.2033C8.37312 25.84 6 23.4669 6 19.6455V12.2033C6 8.37312 8.37312 6 12.2033 6H19.6367ZM11.706 13.4945C11.9073 13.5014 12.0977 13.5879 12.2352 13.7352C12.3726 13.8825 12.4459 14.0784 12.4388 14.2798V20.6226C12.4244 21.0418 12.0728 21.37 11.6536 21.3555C11.2344 21.341 10.9063 20.9895 10.9207 20.5703V14.2187L10.9343 14.1C10.9647 13.9444 11.0439 13.8013 11.162 13.6924C11.3095 13.5564 11.5055 13.4851 11.706 13.4945ZM15.9549 10.5194C16.3741 10.5194 16.7139 10.8592 16.7139 11.2785V20.579C16.7139 20.9982 16.3741 21.338 15.9549 21.338C15.5357 21.338 15.1958 20.9982 15.1958 20.579V11.2785C15.1958 10.8592 15.5357 10.5194 15.9549 10.5194ZM20.1602 16.8448C20.5794 16.8448 20.9193 17.1847 20.9193 17.6039V20.5703C20.9193 20.9895 20.5794 21.3293 20.1602 21.3293C19.741 21.3293 19.4012 20.9895 19.4012 20.5703V17.6039C19.4012 17.1847 19.741 16.8448 20.1602 16.8448Z"
+                          fill="white"
+                          fillOpacity="0.92"
+                        />
+                      </svg>
+                    </i>
+                    Statistics
+                  </div>
+                  <div className={`flex`}>
+                    <div className="w-full flex flex-col sm:flex-row justify-between ">
+                      <div
+                        className={`w-full md:w-full  mr-4  flex flex-col items-center justify-center relative`}
+                      >
+                        <span className="text-[#095ea4] text-2xl font-bold">
+                          {totalAmountSum}
+                        </span>
+                        <h4 className="text-center text-xs whitespace-nowrap">
+                          Total Amount
+                        </h4>
+                      </div>
+                    </div>
+                    <div className="w-full flex flex-col sm:flex-row justify-between ">
+                      <div
+                        className={`w-full md:w-full  mr-4  flex flex-col items-center justify-center relative`}
+                      >
+                        <span className="text-[#1dafc9] text-2xl font-bold">
+                          {totalBillSum}
+                        </span>
+                        <h4 className="text-center text-xs whitespace-nowrap">
+                          Total Customer Count
+                        </h4>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="w-full flex flex-col sm:flex-row justify-between">
+                  <div className={` m-1 flex flex-col relative p-5 w-full`}>
+                    <Chart
+                      options={barchartOptions}
+                      series={barchartOptions.series}
+                      type="line"
+                      height={265}
+                      width={"100%"}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+
           </div>
         </div>
       </div>
